c3809e407c22e057d82994ce4eb7f41a86f137e3
Driesboy <coenendries3@gmail.com>
2017-02-10 20:07:58 +0100

    Update Main.php

--- src/slapper/Main.php
+++ src/slapper/Main.php
@@ -6,9 +6,13 @@ use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\command\ConsoleCommandSender;
 use pocketmine\entity\Entity;
+use pocketmine\event\player\PlayerMoveEvent;
+use pocketmine\math\Vector2;
 use pocketmine\event\entity\EntityDamageByEntityEvent;
 use pocketmine\event\entity\EntityDamageEvent;
 use pocketmine\event\entity\EntitySpawnEvent;
+use pocketmine\network\protocol\MoveEntityPacket;
+use pocketmine\network\protocol\MovePlayerPacket;
 use pocketmine\event\Listener;
 use pocketmine\event\server\DataPacketSendEvent;
 use pocketmine\Item\Item;
@@ -189,6 +193,57 @@ class Main extends PluginBase implements Listener {
         Entity::registerEntity(SlapperFallingSand::class, true);
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
     }
+    
+    public function onPlayerMove(PlayerMoveEvent $ev){
+        $player = $ev->getPlayer();
+        $from = $ev->getFrom();
+        $to = $ev->getTo();
+        if($from->distance($to) < 0.1){
+            return;
+        }
+        $maxDistance = $this->getConfig()->get("max-distance");
+        foreach($player->getLevel()->getNearbyEntities($player->getBoundingBox()->grow($maxDistance, $maxDistance, $maxDistance), $player) as $e){
+            if($e instanceof Player){
+                continue;
+            }
+            if(substr($e->getSaveId(), 0, 7) !== "Slapper"){
+                continue;
+            }
+            if($e->getSaveId() === "SlapperFallingSand"){
+                continue;
+            }
+            $xdiff = $player->x - $e->x;
+            $zdiff = $player->z - $e->z;
+            $angle = atan2($zdiff, $xdiff);
+            $yaw = (($angle * 180) / M_PI) - 90;
+            $ydiff = $player->y - $e->y;
+            $v = new Vector2($e->x, $e->z);
+            $dist = $v->distance($player->x, $player->z);
+            $angle = atan2($dist, $ydiff);
+            $pitch = (($angle * 180) / M_PI) - 90;
+            if($e->getSaveId() === "SlapperHuman"){
+                $pk = new MovePlayerPacket();
+                $pk->eid = $e->getId();
+                $pk->x = $e->x;
+                $pk->y = $e->y + $e->getEyeHeight();
+                $pk->z = $e->z;
+                $pk->yaw = $yaw;
+                $pk->pitch = $pitch;
+                $pk->bodyYaw = $yaw;
+            } else {
+                $pk = new MoveEntityPacket();
+                $pk->eid = $e->getId();
+                $pk->x = $e->x;
+                $pk->y = $e->y + $e->offset;
+                $pk->z = $e->z;
+                $pk->yaw = $yaw;
+                $pk->headYaw = $yaw;
+                $pk->pitch = $pitch;
+            }
+            $player->dataPacket($pk);
+        }
+    }
+
 
     public function onCommand(CommandSender $sender, Command $command, $label, array $args){
         switch(strtolower($command->getName())){
