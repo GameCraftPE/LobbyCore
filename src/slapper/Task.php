7f6478bac450f17405eb9746c8365d4fc0a98180
Driesboy <coenendries3@gmail.com>
2017-02-15 07:46:01 +0100

    Update Task.php

--- src/slapper/Task.php
+++ src/slapper/Task.php
@@ -14,7 +14,7 @@ class Task extends PluginTask{
     $pl = $this->plugin->getServer()->getOnlinePlayers();
       foreach($pl as $p){
         if(!$p->getInventory()->getItemInHand()->hasEnchantments()){
-          $p->sendPopup(TF::GRAY."You are playing on ".TF::BOLD.TF::BLUE."GameCraft PE".TF::RESET."\n".TF::DARK_GRAY."[".TF::LIGHT_PURPLE.count($this->plugin->getServer()->getOnlinePlayers()).TF::DARK_GRAY."/".TF::LIGHT_PURPLE.$this->plugin->getServer()->getMaxPlayers().TF::DARK_GRAY."] | ".TF::BOLD.TF::AQUA."Vote: ".TF::RESET.TF::GREEN."gamecraftvote.tk");
+          $p->sendPopup(TF::GRAY."You are playing on ".TF::BOLD.TF::BLUE."GameCraft PE".TF::RESET."\n".TF::BOLD.TF::AQUA."Vote: ".TF::RESET.TF::GREEN."gamecraftvote.tk");
         }
       }
   }
